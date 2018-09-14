4df5c509ee4a5f8ac4b5119099f934b46514e6a2
Matthew Simpson <matthew@matthewsimpson.net>
2018-09-14 08:29:25 +0100

    Completed first problem.

--- src/problem0001/mod.rs
+++ src/problem0001/mod.rs
@@ -1,3 +1,11 @@
 pub fn main() -> i32 {
-    return 4;
+    let mut sum = 0;
+
+    for value in 1..1000 {
+        if value % 3 == 0 || value % 5 == 0 {
+            sum += value;
+        }
+    }
+
+    return sum;
 }
