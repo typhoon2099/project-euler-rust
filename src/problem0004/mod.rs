902e3708ad574d39ffc4143258b44ad62ef065c8
Matthew Simpson <matthew@matthewsimpson.net>
2019-04-19 23:53:22 +0100

    Exit the inner loop once we've found a palindrome (it'll be the highest we'll find), and exit once the outer loop will be unable to find a higher palindrome.

--- src/problem0004/mod.rs
+++ src/problem0004/mod.rs
@@ -2,10 +2,18 @@ pub fn main() -> u32 {
     let mut answer:u32 = 0;
 
     for x in (100..999).rev() {
-        for y in (x..999).rev() {
+        if x < (answer as f64).sqrt() as u32 {
+            break;
+        }
+
+        for y in (100..999).rev() {
             let mut current = x * y;
-            if is_palindrome(current) && current > answer {
-                answer = current;
+            if is_palindrome(current) {
+                if current > answer {
+                    answer = current;
+                }
+
+                break;
             }
         }
     }
